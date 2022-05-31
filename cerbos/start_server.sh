016782080c387c491aed734278a30b6a8299e97f
Charith Ellawala <charithe@users.noreply.github.com>
2022-05-31 13:48:19 +0100

    [WIP]chore: Upgrade to Cerbos 0.16.0 (#21)
    WIP: `go.mod` must be updated to use Cerbos 0.16.0 when it's released
    
    - Use the new Go SDK
    - Document `cerbos run`
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- cerbos/start_server.sh
+++ cerbos/start_server.sh
@@ -2,10 +2,9 @@
 
 set -euo pipefail
 
-LATEST_VERSION=$(curl --silent "https://api.github.com/repos/cerbos/cerbos/releases/latest" | jq -r .tag_name)
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONTAINER_IMG=${CONTAINER_IMG:-"ghcr.io/cerbos/cerbos"}
-CONTAINER_TAG=${CONTAINER_TAG:-"${LATEST_VERSION:1}"}
+CONTAINER_TAG=${CONTAINER_TAG:-"latest"}
 
 docker run -i -t -p 3592:3592 -p 3593:3593 \
   -v ${SCRIPT_DIR}/policies:/policies \
